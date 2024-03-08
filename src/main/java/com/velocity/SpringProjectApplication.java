e68fa4146ffd5a64b9adfd8ba5966edbd4afad18
utkarsha512 <158495132+utkarsha512@users.noreply.github.com>
2024-03-08 22:56:22 +0530

    Update SpringProjectApplication.java

--- src/main/java/com/velocity/SpringProjectApplication.java
+++ src/main/java/com/velocity/SpringProjectApplication.java
@@ -8,6 +8,7 @@ public class SpringProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringProjectApplication.class, args);
+		System.out.println(10);
 	}
 
 }
